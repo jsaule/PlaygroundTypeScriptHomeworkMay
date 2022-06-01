3ad5d2a782116f63208531bb722475436dad5cfe
Julija Sauleniene <julija.sauleniene@gmail.com>
2022-06-01 11:58:06 +0300

    Homework 9 added

---
+++ Homework 9/public/js/air-vehicle.js
@@ -0,0 +1,11 @@
+import Vehicle from "./vehicle.js";
+class AirVehicle extends Vehicle {
+    constructor({ maxAltitude }, vehicleProps) {
+        super(vehicleProps);
+        this.toString = () => {
+            return `${this.toStringVehicle()}\tmaksimalus aukštis: ${this.maxAltitude}`;
+        };
+        this.maxAltitude = maxAltitude;
+    }
+}
+export default AirVehicle;
---
+++ Homework 9/public/js/land-vehicle.js
@@ -0,0 +1,11 @@
+import Vehicle from "./vehicle.js";
+class LandVehicle extends Vehicle {
+    constructor({ tires }, vehicleProps) {
+        super(vehicleProps);
+        this.toString = () => {
+            return `${this.toStringVehicle()}\tpadangos: ${this.tires.join(', ')}`;
+        };
+        this.tires = tires;
+    }
+}
+export default LandVehicle;
